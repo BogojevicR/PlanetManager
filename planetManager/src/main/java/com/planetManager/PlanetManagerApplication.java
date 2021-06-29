62ec3ccdf0c2bf1f4bf4ffc4aa46b92e56bc0dba
BogojevicR <ralebogojevic@gmail.com>
2021-06-29 20:27:49 +0200

    initial commit

---
+++ planetManager/src/main/java/com/planetManager/PlanetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.planetManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetManagerApplication.class, args);
+	}
+
+}
---
+++ planetManager/src/test/java/com/planetManager/PlanetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
